2089c1af2f92b38f9cd735ccfbf68032e027ad0b
giahuu <84818160+giahuu@users.noreply.github.com>
2022-05-29 05:42:56 +0700

    Update A.sh

--- A.sh
+++ A.sh
@@ -14,8 +14,8 @@ wget -O Windows.iso 'https://dl.malwarewatch.org/windows/Windows%2010%201507.iso
 
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo qemu-system-x86_64 \
-  -m 8G \
-  -cpu core2duo \
+  -m 2G \
+  -cpu EPYC \
   -boot order=d \
   -drive file=Windows.iso,media=cdrom \
   -drive file=windows.img,format=raw \
